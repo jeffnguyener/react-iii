e6ec74480435db6b24fb5bc8c93ab8c7190098b6
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro.local>
2019-05-04 13:31:39 -0600

    update v2.2

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 import './Content.css'
+import data from '../../data'
 
-export default function Contents(props){
-    const mappedUser = props.filteredUser.map(user => (
-        <div>
-           <h1> {user.id}/25</h1>
-        </div>
-    ))
+export default function Contents(props) {
+    const mappedUser = props.filteredUser.map(user => {
+        const userData = data[user.id - 1]
+        return (
+            <div>
+                <h1 className="page-index"> {user.id}/25</h1>
+                <div className="user">
+                    <h1>{userData.name.first} {userData.name.last}
+                    </h1>
+                    <div className="from">
+                        <h1>From</h1>{userData.city}
+                    </div>
+                </div>
+
+            </div>
+        )
+    });
+
+
+
+
 
     return (
         <div className="content-container">
-            <div className="page-count-container">
-                {mappedUser}
-            </div>
+            {mappedUser}
         </div>
     )
 };
